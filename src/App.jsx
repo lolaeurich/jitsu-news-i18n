e4245347aa9213cfd9e98696f4d33494c7fdd6ba
lolaeurich <lola.tatti@gmail.com>
2023-10-19 16:29:09 -0300

    feat: useFetch e postagens do blog no Stripe

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
-import { useState } from 'react'
 import BlogContent from './pages/BlogContent'
 import Home from './pages/Home'
 import {Routes, Route} from "react-router-dom"
-
+import useFetch from "./hooks/useFetch"
 
 function App() {
-  const [count, setCount] = useState(0)
+  let {loading, data, error} = useFetch('http://localhost:1337/api/blogs?populate=*')
+  if(loading) return <p>Loading...</p>
+  if(error) return <p>Error!</p>
+  console.log(data)
 
   return (
     <div>
---
+++ src/hooks/useFetch.js
@@ -0,0 +1,32 @@
+import { useEffect, useState } from 'react'
+
+function useFetch(url) {
+    const [data, setData] = useState(null)
+    const [error, setError] = useState(null)
+    const [loading, setLoading] = useState(true)
+
+    useEffect(() => {
+        const fetchData = async () => {
+            setLoading(true)
+            try {
+
+                const res = await fetch(url)
+                const json = await res.json()
+                setData(json)
+                setLoading(false)
+
+            } catch (error) {
+
+                setError(error)
+                setLoading(false)
+
+            }
+        }
+        fetchData()
+
+    }, [url])
+
+  return {loading, error, data}
+}
+
+export default useFetch
